3e6b260578b0a48ca997281b1598d3bd2e8788a6
cinosh07 <cinosh07@hotmail.com>
2019-09-26 22:48:42 -0400

    Add license notice

--- Arduino/icom_civ.ino
+++ Arduino/icom_civ.ino
@@ -1,4 +1,17 @@
+/*
+   Mag loop antenna auto tuner
 
+   Created 30 March. 2019
+   Updated september 26th 2019
+   by Carl Tremblay - VA2SAJ AKA cinosh07
+
+   Attribution-NonCommercial 2.0 Generic (CC BY-NC 2.0) License
+
+   code and circuitry inspired from
+   https://create.arduino.cc/editor/JMardling/67243a05-af85-4b7b-832c-b67a56f8b77c/preview
+   www.ve1zac.com/Simple%20Loop%20Automatic%20Tuner.pdf
+   https://sites.google.com/site/lofturj/to-automatically-tune-a-magnetic-loop-antenna
+ */
 //***********************************************
 //
 // Serial port initialization for CI-V protocol
