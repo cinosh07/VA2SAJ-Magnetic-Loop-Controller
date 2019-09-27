7b2de3f97848fe5ce9735dbe81adf258bf299656
cinosh07 <cinosh07@hotmail.com>
2019-09-26 22:45:14 -0400

    Adding license notice

--- Arduino/Mag_Loop_Control_V3.ino
+++ Arduino/Mag_Loop_Control_V3.ino
@@ -5,6 +5,8 @@
    Updated september 26th 2019
    by Carl Tremblay - VA2SAJ AKA cinosh07
 
+   Attribution-NonCommercial 2.0 Generic (CC BY-NC 2.0) License
+
    code and circuitry inspired from
    https://create.arduino.cc/editor/JMardling/67243a05-af85-4b7b-832c-b67a56f8b77c/preview
    www.ve1zac.com/Simple%20Loop%20Automatic%20Tuner.pdf
